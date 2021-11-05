beb5f3487208d79f272f253b1a09c6ba93b87212
Will Medders <wmedders@gmail.com>
2021-11-04 22:46:32 -0400

    Add hashes

--- model1-1.rb
+++ model1-1.rb
@@ -13,3 +13,11 @@ likes_black_licorice = false
 #arrays
 people = ["Jim", "Bob", "Clair"]
 hair_colors = ["White", "Black", "Blonde", "Brown"]
+
+#hashes
+toy_count = {"army_men" => 45, "blocks" => 9, "stuffed_animals" => 2}
+book_author = {
+  "Tales of the South Pacific" => "Michener, James",
+  "Things Fall Apart" => "Achebe, Chinua",
+  "The Silent World" => "Cousteau, Jacques-Ives"
+}
